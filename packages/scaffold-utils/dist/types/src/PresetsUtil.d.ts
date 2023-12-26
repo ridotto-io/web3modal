8fe567b4a6a104b2a6ec84e51638456c3b588007
MBar <martin@ridotto.io>
2023-12-26 08:25:24 -0300

    base

--- packages/scaffold-utils/dist/types/src/PresetsUtil.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ConnectorType } from '@ridotto-io/w3-scaffold';
-export declare const PresetsUtil: {
-    ConnectorExplorerIds: Record<string, string>;
-    EIP155NetworkImageIds: Record<string, string>;
-    ConnectorImageIds: Record<string, string>;
-    ConnectorNamesMap: Record<string, string>;
-    ConnectorTypesMap: Record<string, ConnectorType>;
-    WalletConnectRpcChainIds: number[];
-};
